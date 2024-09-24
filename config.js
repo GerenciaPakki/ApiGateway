7abc9ec300148237fa8a05c373d02d2c0ec6577c
jrico@evalua.co <jrico@evalua.co>
2024-09-24 11:53:36 -0500

    Configuracion de conexion

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ module.exports = {
         {
             dev: {
                 PORT: 3000,
-                MONGO_URL: 'mongodb://localhost:27017/apiGateway',
+                MONGO_URL: 'mongodb://127.0.0.1:27017/apiGateway',
                 MONGO_PORT: 27017,
-                MONGO_USR: '',
-                MONGO_PWD: ''
+                MONGO_USR: 'devappakki',
+                MONGO_PWD: '4Pp4kk1'
             }
         },
         {
