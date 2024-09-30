5f5b5c6890ea4550a3970b7c67d55d587c699d22
jrico@evalua.co <jrico@evalua.co>
2024-09-30 09:30:44 -0500

    Conexion a DB

--- src/data/dbConn.js
+++ src/data/dbConn.js
@@ -17,23 +17,50 @@ const pwd = config.apps[0].dev.MONGO_PWD
 //     }
 // }
 
-async function conn() {
-    try {
-        const options = {
-            auth: { username: usr, password: pwd },
-            // authSource: 'admin', // Dependiendo de cómo esté configurada tu base de datos
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true
-        };
+// async function conn() {
+//     try {
+//         const options = {
+//             auth: { username: usr, password: pwd },
+//             // authSource: 'admin', // Dependiendo de cómo esté configurada tu base de datos
+//             // useNewUrlParser: true,
+//             // useUnifiedTopology: true
+//         };
         
-        // Si no hay usuario, no necesitas opciones de autenticación
-        await mongoose.connect(mongoURL, options);
-        return mongoose.connection;
+//         // Si no hay usuario, no necesitas opciones de autenticación
+//         await mongoose.connect(mongoURL, options);
+//         return mongoose.connection;
+//     } catch (error) {
+//         console.error('Error en la función conn:', error);
+//         throw new Error('Error conectando a la base de datos');
+//     }
+// }
+
+const conn = async() => {
+   
+    try {
+        // const uri = "mongodb+srv://dev_pakki:T3cn0l0g14*@atlascluster.3v6nhtw.mongodb.net/devpakkiDB";
+        const uri = `${mongoURL}`;
+        // process.env.DB_CNN
+        mongoose.set("strictQuery", false);
+
+        // Agregar las opciones de autenticación con el usuario y contraseña
+        const options = {
+        auth: {
+            user: `${user}`,
+            password: `${pwd}`
+        }
+        };
+    
+        await mongoose.connect(uri);
+        // console.log('Connection DB');
+        
     } catch (error) {
-        console.error('Error en la función conn:', error);
-        throw new Error('Error conectando a la base de datos');
+        console.log(error);
+        throw new Error('error connecting to database');
+        
     }
-}
+
+};
 
 
 // Exportar la conexión para usarla en otros módulos de tu aplicación
