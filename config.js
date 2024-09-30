d3ee4c61c41799c335f2dd525f030595a244fd8c
jrico@evalua.co <jrico@evalua.co>
2024-09-30 11:10:53 -0500

    Conexion DB

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             dev: {
                 PORT: 3000,
-                MONGO_URL: 'mongodb://127.0.0.1:27017/devpakkiDB',
+                MONGO_URL: 'mongodb://127.0.0.1:27017/apiGateway',
                 MONGO_PORT: 27017,
                 MONGO_USR: 'devappakki',
                 MONGO_PWD: '4Pp4kk1'
