6e50e6b28ff16cb657b24540ee7cdd0384d12613
jrico@evalua.co <jrico@evalua.co>
2024-09-30 07:56:39 -0500

    Conexion a DB

--- src/data/dbConn.js
+++ src/data/dbConn.js
@@ -5,10 +5,28 @@ const mongoURL = config.apps[0].dev.MONGO_URL // 'mongodb://localhost:27017/apiG
 const usr = config.apps[0].dev.MONGO_USR
 const pwd = config.apps[0].dev.MONGO_PWD
 
+// async function conn() {
+//     try {        
+//         console.log(usr,pwd);
+//         const options = {auth: {user: usr,password: pwd}};
+//         await mongoose.connect(mongoURL, usr === '' ? undefined : options);
+//         return mongoose.connection;
+//     } catch (error) {
+//         console.error('Error en la función conn:', error);
+//         throw new Error('Error conectando a la base de datos');
+//     }
+// }
+
 async function conn() {
-    try {        
-        console.log(usr,pwd);
-        const options = {auth: {user: usr,password: pwd}};
+    try {
+        const options = {
+            auth: { username: usr, password: pwd },
+            authSource: 'admin', // Dependiendo de cómo esté configurada tu base de datos
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        };
+        
+        // Si no hay usuario, no necesitas opciones de autenticación
         await mongoose.connect(mongoURL, usr === '' ? undefined : options);
         return mongoose.connection;
     } catch (error) {
@@ -16,6 +34,7 @@ async function conn() {
         throw new Error('Error conectando a la base de datos');
     }
 }
+
 
 // Exportar la conexión para usarla en otros módulos de tu aplicación
 module.exports = conn;
